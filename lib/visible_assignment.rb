7a954d03e7138689b269a271a10512f1fb2d7638
h1kita <shunhikita@gmail.com>
2019-03-03 22:43:26 +0900

    include extension when action_controller loading

--- lib/visible_assignment.rb
+++ lib/visible_assignment.rb
@@ -1,6 +1,13 @@
 require "visible_assignment/version"
 
+require 'active_support'
+require 'visible_assignment/config'
+require 'visible_assignment/action_controller_extension'
+
 module VisibleAssignment
   class Error < StandardError; end
-  # Your code goes here...
+
+  ActiveSupport.on_load(:action_controller) do
+    include ActionControllerExtension
+  end
 end
