af16586f010231c89cd5e07d88a1dd756a80dd15
h1kita <shunhikita@gmail.com>
2019-03-03 10:09:30 +0900

    initial commit

---
+++ lib/visible_assignment.rb
@@ -0,0 +1,6 @@
+require "visible_assignment/version"
+
+module VisibleAssignment
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/visible_assignment_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe VisibleAssignment do
+  it "has a version number" do
+    expect(VisibleAssignment::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
